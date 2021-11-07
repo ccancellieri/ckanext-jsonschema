ec62db7fc90d3ab594586fdb24ecd584a9f0ba0d
carlo cancellieri <ccancellieri@hotmail.com>
2021-11-07 22:37:38 +0100

    introduce harvester

--- ckanext/jsonschema/__init__.py
+++
@@ -1,4 +0,0 @@
-import ckanext.jsonschema.iso19139.harvester as h
-HarvesterIso19139 = h.HarvesterIso19139 
-import ckanext.jsonschema.iso19139.iso19139 as iso19139
-JsonschemaIso19139 = iso19139.JsonschemaIso19139
