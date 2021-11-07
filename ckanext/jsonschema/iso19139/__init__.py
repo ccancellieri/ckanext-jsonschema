3123ffd86be77bad000ed84f68ec97f0a9e55c6f
carlo cancellieri <ccancellieri@hotmail.com>
2021-11-07 23:11:10 +0100

    introduce harvester

--- ckanext/jsonschema/iso19139/__init__.py
+++ ckanext/jsonschema/iso19139/__init__.py
@@ -1,2 +1,4 @@
-# from ckanext.jsonschema.iso19139.harvester import HarvesterIso19139 
-# from ckanext.jsonschema.iso19139.iso19139 import JsonschemaIso19139
+from ckanext.jsonschema.iso19139.harvester import HarvesterIso19139 
+from ckanext.jsonschema.iso19139.iso19139 import JsonschemaIso19139
+
+__all__ = [HarvesterIso19139, JsonschemaIso19139]
