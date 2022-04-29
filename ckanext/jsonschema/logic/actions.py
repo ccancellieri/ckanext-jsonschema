d4e5a3501d796ad45f372aad1530973f654e3e18
Gianfranco Demarco <g.demarco@reply.it>
2022-04-29 10:24:47 +0200

    FIX get view resolved

--- ckanext/jsonschema/logic/actions.py
+++ ckanext/jsonschema/logic/actions.py
@@ -292,7 +292,7 @@ def view_show(context, data_dict):
     view_document = _t.dictize_pkg(json.loads(view_document.get('view_jsonschemas')[idx]))
 
     if resolve.lower() == "true":
-        view_body = view_document.get(_c.SCHEMA_BODY_KEY)
+        view_body = view_document.get('{}_resolved'.format(_c.SCHEMA_BODY_KEY))
     else:
         view_body = view_document.get(_c.SCHEMA_BODY_KEY) 
 
@@ -332,8 +332,8 @@ def spatial_search(context, data_dict):
     search_params = _it.params_for_solr_search(bbox_query_validated, search_params)
 
     # {!frange incl=false l=0 u=1}div(mul(mul(max(0,sub(min(180,maxx),max(-180,minx))),max(0,sub(min(90,maxy),max(-90,miny)))),2),add(0.0,mul(sub(maxy,miny),sub(maxx,minx))))
-# 'div(mul(mul(max(0,sub(min(152.45628,maxx),max(-12.311967,minx))),max(0,sub(min(15.993704,maxy),max(28.187208,miny)))),2),add(2009.10227887,mul(sub(maxy,miny),sub(maxx,minx))))'
-
+    # 'div(mul(mul(max(0,sub(min(152.45628,maxx),max(-12.311967,minx))),max(0,sub(min(15.993704,maxy),max(28.187208,miny)))),2),add(2009.10227887,mul(sub(maxy,miny),sub(maxx,minx))))'
+    # https://github.com/dsmiley/SOLR-2155
     results = indexer.search_with_params_dict(search_params)
 
     return results
