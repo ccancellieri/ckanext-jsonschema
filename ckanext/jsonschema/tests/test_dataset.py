2fee270a80e27320b35107c5968ce6bdeaf437cb
Gianfranco Demarco <g.demarco@reply.it>
2022-05-05 17:58:25 +0200

    More tests

--- ckanext/jsonschema/tests/test_dataset.py
+++ ckanext/jsonschema/tests/test_dataset.py
@@ -12,6 +12,9 @@ import ckanext.jsonschema.constants as _c
 import pytest
 from six import text_type
 
+from logging import getLogger
+log = getLogger(__name__)
+
 
 # Runs before each test
 @pytest.fixture(autouse=True)
@@ -92,9 +95,84 @@ class TestDataset(object):
         assert json.loads(cloned_json_resource) == source_json_resource
 
         
-    def test_clone_api_with_normal_user(self):
-        assert True
-    
+    def test_clone_api_with_normal_user(self, dataset_sample3, app):
+        # We create a package
+        # Then we try to clone it by a user with edit permission on its organization, and we should succeed
+        # Then we try to clone it by a user without the needed permissions, and we should fail
+
+        
+        # Create the user
+        user1 = factories.User()
+        user2 = factories.User()
+
+        # Create organization with user1 as editor
+        owner_org = factories.Organization(
+            users=[{'name': user1.get('id'), 'capacity': 'editor'}]
+        )
+
+        # Create the metadata in that organization
+        package_dict = {
+            'owner_org': owner_org.get('id'),
+            'name': str(uuid.uuid4())
+        }
+        package_dict.update(dataset_sample3)
+
+        context = {'user': user1.get('name')}
+
+        # Create with the source package with the first user
+        package = toolkit.get_action('package_create')(context, package_dict)
+
+
+        # Create headers and payload for the clone request with the first user (should succeed)
+        headers = {'Authorization': str(user1.get('apikey'))}
+        data_dict = {
+            'id': package['id'],
+            'owner_org': owner_org['id']
+        }
+
+        # Request the clone
+        response = self.__do_post(app, 
+            '/api/action/jsonschema_clone', 
+            data=data_dict,
+            headers=headers
+        )
+
+        response_body = json.loads(response.body)
+        assert response_body['success'] == True
+
+        # The id should be different from the source package
+        cloned_id = response_body['result']['id']
+        assert cloned_id != package['id']
+
+        # Check that we can do a package show
+        show_result = toolkit.get_action('package_show')(context, {'id': cloned_id})
+        assert show_result != None
+
+        # Create headers and payload for the clone request with the second user (should fail)
+        headers = {'Authorization': str(user2.get('apikey'))}
+        data_dict = {
+            'id': package['id'],
+            'owner_org': owner_org['id']
+        }
+
+        # Request the clone
+        try:
+             # Request the clone
+            response = self.__do_post(app, 
+                '/api/action/jsonschema_clone', 
+                data=data_dict,
+                headers=headers
+            )     
+        # except webtest.app.AppError as e:
+        except Exception as e:
+            # Testing the string message is not very reliable, but we need to be sure that the
+            # error is 403 and not anything else, and there isn't the response status in the object
+            log.info(e.message)
+            if e.message.startswith('Bad response: 403'):
+                assert True
+            else:
+                assert False
+
     ################################################
 
 
